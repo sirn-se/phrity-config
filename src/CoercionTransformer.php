f8c5f1bdc235db6e78b07760289898388dd7c303
Sören Jensen <sirn@sirn.se>
2025-07-29 10:08:25 +0200

    Transformer improvements

--- src/CoercionTransformer.php
+++ src/CoercionTransformer.php
@@ -3,16 +3,30 @@
 namespace Phrity\Config;
 
 use Phrity\Util\Transformer\{
-    TransformerInterface,
     BasicTypeConverter,
-    Type,
+    FirstMatchResolver,
+    ReadableConverter,
+    ReversedReadableConverter,
+    TransformerInterface,
 };
 
-class CoercionTransformer extends BasicTypeConverter implements TransformerInterface
+class CoercionTransformer implements TransformerInterface
 {
+    private FirstMatchResolver $resolver;
+
+    public function __construct()
+    {
+        $this->resolver = new FirstMatchResolver([
+            new ReadableConverter(),
+            new ReversedReadableConverter(),
+            new BasicTypeConverter(),
+        ]);
+    }
+
+
     public function canTransform(mixed $subject, string|null $type = null): bool
     {
-        if (!parent::canTransform($subject, $type)) {
+        if (!$this->resolver->canTransform($subject, $type)) {
             throw new CoercionException("Invalid coercion type '{$type}'");
         }
         return true;
@@ -20,22 +34,7 @@ class CoercionTransformer extends BasicTypeConverter implements TransformerInter
 
     public function transform(mixed $subject, string|null $type = null): mixed
     {
-        parent::canTransform($subject, $type);
-        $subjectType = gettype($subject);
-        if ($type == Type::STRING && in_array($subjectType, [Type::BOOLEAN, Type::NULL])) {
-            return match ($subjectType) {
-                Type::BOOLEAN => $subject ? 'true' : 'false',
-                Type::NULL => 'null',
-            };
-        }
-        if ($type == Type::BOOLEAN && $subjectType == Type::STRING) {
-            return match (strtolower($subject)) {
-                'true', '1' => true,
-                'false', '0', '' => false,
-                default => (bool)$subject,
-            };
-        }
-
-        return parent::transform($subject, $type);
+        $this->canTransform($subject, $type);
+        return $this->resolver->transform($subject, $type);
     }
 }
